477764ce421ff2f519f246843d5e79f25c974a7c
Davaille-Leroy Matisse <mat.dl17000@gmail.com>
2022-04-01 16:35:43 +0200

    maj

--- portfolio_mdl/src/data/PersonnalData.js
+++ portfolio_mdl/src/data/PersonnalData.js
@@ -8,4 +8,13 @@ export const PersonnalData = [
 		info: `Réalisation d'un site vitrine pour une graphiste Lyonnaise.`,
 		picture: './media/fdl.jpg',
 	},
+	{
+		id: 2,
+		name: 'Crypto Bible',
+		languages: ['react', 'antd'],
+		languagesIcons: ['./media/ant.png', './media/react.png'],
+		source: 'https://crypto-bible.netlify.app/',
+		info: `Réalisation d'une site de suivi de cryptomonnaies avec React / Redux / Ant Design. Utilisation d'API.`,
+		picture: './media/crypto-bible.png',
+	},
 ];
